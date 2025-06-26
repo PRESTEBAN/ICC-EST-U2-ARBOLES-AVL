b0de2a36a9c178531031fee97b0318bf10564f2f
PRESTEBAN <145607969+PRESTEBAN@users.noreply.github.com>
2025-06-26 08:46:40 -0500

    FINAL

---
+++ src/AVLTrre.java
@@ -0,0 +1,87 @@
+public class AVLTrre {
+    
+    private Node root;
+
+
+    public AVLTrre(){
+        this.root = null;
+    }
+
+    public int height(Node node){
+           if (node == null) {
+               return 0;
+           }
+           return node.getHeight();
+    }
+
+    public int getBalance(Node node){
+        if (node == null) {
+            return 0;
+        }
+
+        return height(node.getLeft()) - height(node.getRight());
+    }
+
+    public void insert(int value){
+        System.out.println("Valor a insertar " + value);
+        root = insertRec(root, value);
+    }
+
+    public Node insertRec(Node node, int value){
+
+       if(node == null){
+         Node newNode = new Node(value);
+         newNode.setHeight(1);
+         System.out.println("Nodo insertado: " + newNode.getValue() + " balance al insertar = " + getBalance(newNode));
+         return newNode;
+       }
+
+       if (value < node.getValue()) {
+           node.setLeft(insertRec(node.getLeft(), value));
+       }else if (value > node.getValue()) {
+           node.setRight(insertRec(node.getRight(), value));
+       }else{
+          return node;
+       }
+
+       System.out.println("Node actual" + node.getValue());
+
+       // actualizar la altura de este ancestro node
+
+       int altura = 1 + Math.max(height(node.getLeft()), height(node.getRight()));
+       node.setHeight(altura);
+
+       System.out.println("\tAltura = " + node.getHeight());
+
+       int balance = getBalance(node);
+       System.out.println("\tBalance = " + balance);
+
+       //caso Izquierda - Izquierda
+       if (balance > 1 && value < node.getLeft().getValue()) {
+           System.out.println("Rotacion Derecha");
+       }
+
+       //caso derecha derecha
+       if (balance < -1 && value > node.getRight().getValue()) {
+           System.out.println("Rotacion Izquierda");
+       }
+
+       //Caso Izquierda - Derecha
+        if ((balance > 1 && value > node.getLeft().getValue()) ) {
+            System.out.println("Cambio");
+            System.out.println("Rotacion Izquierda derecha");
+       }
+
+       // Caso Derecha - Izquierda
+     if (balance < -1 && value < node.getRight().getValue()) {
+         System.out.println("Cambio");
+        System.out.println("Rotación Derecha-Izquierda");
+    }   
+
+
+       return node;
+    }
+
+
+
+}
--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Nombres: ESTEBAN PESANTEZ - CARLOS GORDILLO");
+        AVLTrre arbol = new AVLTrre();
+        arbol.insert(10);
+        arbol.insert(20);
+        arbol.insert(15);
     }
 }
